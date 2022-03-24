cb953523610f1377605081d39b67a28d5f051be1
serfinxx <serfinxx@gmail.com>
2022-03-24 20:54:23 +0000

    Upload shell

--- adapet-shell/adapet-num/ada_bert_10_p0.sh
+++ adapet-shell/adapet-num/ada_bert_10_p0.sh
@@ -1,3 +1,2 @@
-cd /home/acb19lh/diss21/ADAPET-master/
 source bin/setup.sh
 python cli.py --data_dir /home/acb19lh/diss21/ADAPET-master/magpie-corpus-master/10/ --pattern '[TEXT1], The sentence is [LBL].' --dict_verbalizer '{"l": "literal", "i": "phrase"}'
