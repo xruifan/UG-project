8d1963134c0d7714db56bbad9b3e275e3af8cfb6
serfinxx <serfinxx@gmail.com>
2022-03-26 02:04:04 +0000

    Cancelled virtual env

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,4 @@
-source env/bin/activate
+#source env/bin/activate
 # Changed root to absolute path    Xuan-Rui Fan
 export ADAPET_ROOT="/home/acb19lh/diss21/ADAPET-master"
 export PYTHONPATH=$ADAPET_ROOT:$PYTHONPATH
