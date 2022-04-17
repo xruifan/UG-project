0691683fa7c2dab331183ec62023fd63d258ddea
serfinxx <74106382+serfinxx@users.noreply.github.com>
2022-04-17 08:16:06 +0100

    Changed metrics to f1 score

--- run_classifier.py
+++ run_classifier.py
@@ -3,37 +3,39 @@ Author: Xuan-Rui Fan
 Email: serfinxx@gmail.com
 Date: 13 Feb 2022
 """
-from sre_parse import FLAGS
 import tensorflow as tf
+import tensorflow_addons as tfa
 import os
 
 from model import Model
 from data_processor import DataProcessor
 from tokenizer import Tokenizer
 from tensorflow.python.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras import backend as K
 
 import json
-import pandas as pd
+
 
 class Run:
     def __init__(self):
         # enable xla devices
         os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
-
-        self.DATA_DIR = "./magpie-corpus-master"
-
+        
+        # need to change DATA_DIR and config for different tasks
         self.config = { 'model_name': 'bert_idiom_finetuned',
+                        'train_num' : 19280,
                         'label_list' : [0, 1], 
                         'max_seq_length' : 500, # maximum length of (token) input sequences
-                        'learning_rate': 3e-5,
+                        'learning_rate': 1e-5,
+                        'epsilon': 1e-8,
                         'epochs': 3,
                         'optimizer': 'adam',
-                        'regularization_penalty': 6e-4,
                         'train_batch': 2,
-                        'dev_batch': 8,
-                        'test_batch': 8,
+                        'dev_batch': 4,
+                        'test_batch': 2,
                 }
-        
+
+        self.DATA_DIR = "./magpie-corpus-master/{}".format(self.config['train_num'])
         self.handle = 'https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/2'
 
     def run(self):
@@ -53,19 +55,34 @@ class Run:
 
         model = classification_model.create_model()
 
-        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=self.config['learning_rate']),
+        # define metrics
+        def f1_macro(y_true, y_pred):
+            def recall(y_true, y_pred):
+                true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+                possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+                recall = true_positives / (possible_positives + K.epsilon())
+                return recall
+
+            def precision(y_true, y_pred):
+                true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+                predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+                precision = true_positives / (predicted_positives + K.epsilon())
+                return precision
+
+            p = precision(y_true, y_pred)
+            r = recall(y_true, y_pred)
+            return 2*((p*r)/(p+r+K.epsilon()))
+
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=self.config['learning_rate'],epsilon=self.config['epsilon']),
                     loss=tf.keras.losses.BinaryCrossentropy(),
-                    metrics=[tf.keras.metrics.BinaryAccuracy(),
-                            tf.keras.metrics.PrecisionAtRecall(0.5),
-                            tf.keras.metrics.Precision(),
-                            tf.keras.metrics.Recall()])
+                    metrics = [f1_macro, 'acc'])
 
         model.summary()
 
         # callbacks
-        checkpoint = ModelCheckpoint(os.path.dirname(os.path.abspath(__file__)), monitor='val_loss', verbose=1, save_best_only=True, mode='max')
+        #checkpoint = ModelCheckpoint(os.path.dirname(os.path.abspath(__file__)), monitor='val_loss', verbose=1, save_best_only=True, mode='max')
         early_stopping = EarlyStopping(monitor='val_loss', patience=2),
-        callbacks_list = [checkpoint, early_stopping]
+        callbacks_list = [early_stopping]
 
         # train model
         history = model.fit(train_data,
@@ -83,14 +100,14 @@ class Run:
             os.makedirs(results_folder_path)
 
         # draw graph
-        Model.create_graphs(history, results_folder_path)
+        #Model.create_graphs(history, results_folder_path)
 
         # write result
-        with open(os.path.join(results_folder_path,'results.txt'), 'w') as file:
+        with open(os.path.join(results_folder_path,f"result_{self.config['train_num']}.txt"), 'w') as file:
             file.write(json.dumps(evaluation_results))
 
         # save model
-        model.save(f"{self.config['model_name']}.h5")
+        model.save(f"{self.config['model_name']}_{self.config['train_num']}.h5")
         
 
 if __name__ == "__main__":
